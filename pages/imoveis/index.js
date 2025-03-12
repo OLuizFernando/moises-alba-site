ab546dd68b4ea7bb38bb7021baf8d26cb872d15e
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2025-03-12 02:04:29 +0000

    add example pages

---
+++ pages/imoveis/index.js
@@ -0,0 +1,10 @@
+function Imoveis() {
+  return (
+    <>
+      <h1>Imóveis</h1>
+      <p>Outro exemplo</p>
+    </>
+  );
+}
+
+export default Imoveis;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <>
+      <h1>Moisés Alba Jr.</h1>
+      <h3>Corretor de Imóveis</h3>
+    </>
+  );
+}
+
+export default Home;
