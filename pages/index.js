9ca031e99f0c29794198fc7af838c0328d7e5951
CodeKeijoo <fernandokdomingos@gmail.com>
2025-03-18 02:01:27 +0000

    create navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
+import Navbar from "@/components/Navbar";
+
 function Home() {
   return (
     <>
+      <Navbar />
       <h1>Moisés Alba Jr.</h1>
       <h3>Compre meu imoveis</h3>
     </>
   );
 }
+
 
 export default Home;
