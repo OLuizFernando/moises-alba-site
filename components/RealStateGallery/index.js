cee9cd57e1952073364780c13263fb32e1681825
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2025-03-21 17:04:43 +0000

    add `<a>` tag that goes to WhatsApp

--- components/RealStateGallery/index.js
+++ components/RealStateGallery/index.js
@@ -31,27 +31,32 @@ function RealStateGallery() {
         </a>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 md:gap-y-16 mt-2 md:mt-5 lg:mt-9 mx-5 md:mx-8 lg:mx-12">
           {/* Residencial Ville Belle */}
-          <div
-            className="flex flex-col h-full justify-center bg-amber-400 rounded-lg p-7"
-            id="ville-belle"
+          <a
+            href="https://wa.me/5511933940598?text=Ol%C3%A1,%20vim%20pelo%20site!%20Gostaria%20de%20saber%20mais%20sobre%20o%20Residencial%20Belle%20Ville"
+            target="_blank"
           >
-            <div className="mb-5">
-              <h1 className="text-5xl font-bold text-white mb-3">
-                Residencial Ville Belle
-              </h1>
-              <h2 className="text-2xl font-medium text-white">
-                Pronto para morar
-              </h2>
+            <div
+              className="flex flex-col h-full justify-center bg-amber-400 hover:bg-amber-300 rounded-lg p-7"
+              id="ville-belle"
+            >
+              <div className="mb-5">
+                <h1 className="text-5xl font-bold text-white mb-3">
+                  Residencial Ville Belle
+                </h1>
+                <h2 className="text-2xl font-medium text-white">
+                  Pronto para morar
+                </h2>
+              </div>
+              <div className="hidden md:block border-t border-white border-2 rounded-full w-1/3 mb-5"></div>
+              <div className="hidden md:block">
+                <ul className="text-white font-medium text-lg">
+                  <li>30m², 46m² e 47m²</li>
+                  <li>1 ou 2 Dorms</li>
+                  <li>Com vaga</li>
+                </ul>
+              </div>
             </div>
-            <div className="hidden md:block border-t border-white border-2 rounded-full w-1/3 mb-5"></div>
-            <div className="hidden md:block">
-              <ul className="text-white font-medium text-lg">
-                <li>30m², 46m² e 47m²</li>
-                <li>1 ou 2 Dorms</li>
-                <li>Com vaga</li>
-              </ul>
-            </div>
-          </div>
+          </a>
           <div>
             <img
               className="h-full max-w-full rounded-lg"
@@ -72,27 +77,32 @@ function RealStateGallery() {
             />
           </div>
           {/* Sol Connection Mooca */}
-          <div
-            className="flex flex-col h-full justify-center bg-amber-400 rounded-lg p-7"
-            id="connection-mooca"
+          <a
+            href="https://wa.me/5511933940598?text=Ol%C3%A1,%20vim%20pelo%20site!%20Gostaria%20de%20saber%20mais%20sobre%20o%20Sol%20Connection%20Mooca"
+            target="_blank"
           >
-            <div className="mb-5">
-              <h1 className="text-5xl font-bold text-white mb-3">
-                Sol Connection Mooca
-              </h1>
-              <h2 className="text-2xl font-medium text-white">
-                Futuro Lançamento
-              </h2>
+            <div
+              className="flex flex-col h-full justify-center bg-amber-400 hover:bg-amber-300 rounded-lg p-7"
+              id="connection-mooca"
+            >
+              <div className="mb-5">
+                <h1 className="text-5xl font-bold text-white mb-3">
+                  Sol Connection Mooca
+                </h1>
+                <h2 className="text-2xl font-medium text-white">
+                  Futuro Lançamento
+                </h2>
+              </div>
+              <div className="hidden md:block border-t border-white border-2 rounded-full w-1/3 mb-5"></div>
+              <div className="hidden md:block">
+                <ul className="text-white font-medium text-lg">
+                  <li>29m² a 56m²</li>
+                  <li>1 ou 2 dorms</li>
+                  <li>Opção com terraço</li>
+                </ul>
+              </div>
             </div>
-            <div className="hidden md:block border-t border-white border-2 rounded-full w-1/3 mb-5"></div>
-            <div className="hidden md:block">
-              <ul className="text-white font-medium text-lg">
-                <li>29m² a 56m²</li>
-                <li>1 ou 2 dorms</li>
-                <li>Opção com terraço</li>
-              </ul>
-            </div>
-          </div>
+          </a>
           <div>
             <img
               className="h-full max-w-full rounded-lg"
@@ -113,25 +123,30 @@ function RealStateGallery() {
             />
           </div>
           {/* Sol Clube Maia */}
-          <div
-            className="flex flex-col h-full justify-center bg-amber-400 rounded-lg p-7"
-            id="clube-maia"
+          <a
+            href="https://wa.me/5511933940598?text=Ol%C3%A1,%20vim%20pelo%20site!%20Gostaria%20de%20saber%20mais%20sobre%20o%20Sol%20Clube%20Maia"
+            target="_blank"
           >
-            <div className="mb-5">
-              <h1 className="text-5xl font-bold text-white mb-3">
-                Sol Clube Maia
-              </h1>
-              <h2 className="text-2xl font-medium text-white">Lançamento</h2>
+            <div
+              className="flex flex-col h-full justify-center bg-amber-400 hover:bg-amber-300 rounded-lg p-7"
+              id="clube-maia"
+            >
+              <div className="mb-5">
+                <h1 className="text-5xl font-bold text-white mb-3">
+                  Sol Clube Maia
+                </h1>
+                <h2 className="text-2xl font-medium text-white">Lançamento</h2>
+              </div>
+              <div className="hidden md:block border-t border-white border-2 rounded-full w-1/3 mb-5"></div>
+              <div className="hidden md:block">
+                <ul className="text-white font-medium text-lg">
+                  <li>41m², 46m² e 58m²</li>
+                  <li>2 ou 3 dorms com suíte, terraço</li>
+                  <li>Com 1 ou 2 vagas</li>
+                </ul>
+              </div>
             </div>
-            <div className="hidden md:block border-t border-white border-2 rounded-full w-1/3 mb-5"></div>
-            <div className="hidden md:block">
-              <ul className="text-white font-medium text-lg">
-                <li>41m², 46m² e 58m²</li>
-                <li>2 ou 3 dorms com suíte, terraço</li>
-                <li>Com 1 ou 2 vagas</li>
-              </ul>
-            </div>
-          </div>
+          </a>
           <div>
             <img
               className="h-full max-w-full rounded-lg"
