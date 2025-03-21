fd028d7b25baf56ed5a1f3469c21b7b1b704b3c1
CodeKeijoo <fernandokdomingos@gmail.com>
2025-03-21 19:30:58 +0000

    moises

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,16 @@ function Home() {
       <div className="mb-[68px]">
         <Navbar />
       </div>
+      <a href='/imoveis'>
       <Carousel>
         {carouselImages.map((image) => (
           <img src={image} />
         ))}
       </Carousel>
+      </a>
+      <div className="bg-neutral-800 py-10 px-0">
+        <img src='/moises_albajr/moises1.png'/>
+      </div>
       <Footer />
     </>
   );
