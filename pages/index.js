471ac94f4e04de3d84f00e34d3da727a716ca302
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2025-03-25 19:16:23 +0000

    add little `about` section to `Home`

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,29 @@ function Home() {
       <div className="mb-[68px]">
         <Navbar />
       </div>
-      <a href='/imoveis'>
-      <Carousel>
-        {carouselImages.map((image) => (
-          <img src={image} />
-        ))}
-      </Carousel>
+      <a href="/imoveis">
+        <Carousel>
+          {carouselImages.map((image) => (
+            <img src={image} />
+          ))}
+        </Carousel>
       </a>
-      <div className="bg-neutral-800 py-10 px-0">
-        <img src='/moises_albajr/moises1.png'/>
+      <div className="bg-neutral-800">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto pb-15 pt-20">
+          <img
+            className="mx-auto mb-10 md:mb-auto me-auto md:me-40 w-[350px] md:w-[400px]"
+            src="/moises_albajr/moises1.png"
+          />
+          <div className="text-white flex flex-col text-center md:text-start px-7">
+            <h1 className="text-5xl mb-5">Sou Moisés Alba</h1>
+            <p className="text-xl max-w-xl">
+              Sou corretor de imóveis e meu objetivo é ajudar você a realizar o
+              sonho do imóvel próprio. Com experiência e dedicação, busco
+              oferecer as melhores opções para que você faça uma escolha segura
+              e tranquila, sempre alinhado às suas necessidades e expectativas.
+            </p>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
